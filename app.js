1c325ee1216d740caee0c96870d440430d8541c4
malik.imenov <imenov-a@mail.ru>
2022-06-12 18:43:52 +0600

    added list app.js

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const {list} = require('./src/list')
 const app = express()
 const PORT = 3000
 
 app.use(express.json())
 
 
-app.get('/api/list', (req, res) => {
-
+app.get('/api/list', async(req, res) => {
+    const {phone_number, hash_numbe} = req.body
+    const result = await list(phone_number, hash_numbe)
+    return res.json(result)
 })
